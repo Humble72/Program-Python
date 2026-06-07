a80a674715b7116af7c8b0288c419ce3a6b74da2
Humble72 <vdi-terminal@EDU-WSB.local>
2026-06-07 09:31:39 +0200

    Dodanie plików logiki i testów

---
+++ logic/calculator.py
@@ -0,0 +1,6 @@
+def calculate_fuel_cost(liters: float, price_per_liter: float) -> float:
+
+    if liters < 0 or price_per_liter < 0:
+        raise ValueError("Ilość litrów i cena nie mogą być ujemne.")
+    
+    return round(liters * price_per_liter, 2)
